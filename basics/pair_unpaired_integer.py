c1c10e3f8194e8e3bd6168b086bdc5789a501d97
Lucas Cavalare <lucascavalare@gmail.com>
2019-04-21 19:43:00 +0200

    Create pair_unpaired_integer.py

---
+++ basics/pair_unpaired_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+""" 
+   A basic approach of inputing a number and outputing the pairs mulitple of 2. 
+   
+   If you desire a mulitple of 3, simply uncomment # the commented line and comment the respectives below them.
+"""
+
+# unpaired = int(input("Give a unpaired number: "))
+pair = int(input("Give a pair number: "))
+# x = 1
+x = 0
+while x <= pair:
+    # print (x * 3)
+    print (x * 2)
+    x = x + 1

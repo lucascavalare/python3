e531cd94333ed92883d38bc70382850fe77b80c2
Lucas Cavalare <lucascavalare@gmail.com>
2019-09-03 19:18:54 +0100

    Creating metadata about distribution.

---
+++ nester/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+        name           = 'nester',
+        version        = '1.0.0',
+        py_module      = ['nester'],
+        author         = 'lfcpython',
+        author_email   = 'lucascavalare@gmail.com',
+        url            = ''
+        description.   = 'A simple printer of nested lists',
+     )  

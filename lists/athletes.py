70549feb325ea0a1549b41b7b0c0606787261bc6
Lucas Cavalare <lucascavalare@gmail.com>
2019-09-30 19:08:30 +0100

    Integrating Content.

---
+++ lists/athletes.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+''' Function to transform the data in the list when finding chars to make the sorted method difficult to work. 
+    The function processes the string to replace any dashes or colons found and returns the sanitized string.'''
+def sanitize(time_string):
+    if '-' in time_string:
+        splitter = '-'
+    elif ':' in time_string:
+        splitter = ':'
+    else:
+        return(time_string)
+    (mins, secs) = time_string.split(splitter)
+    return(mins + '.' + secs)
+
+
+''' Open each of the data files in turn, read the line of data from the file, 
+    and create a list from the line of data.''' 
+with open('james.txt') as James:        # Open the file.
+    data1 = James.readline()            # Read the line of data.
+    James = data1.strip().split(',')    # Convert the data to a list.
+
+with open('julie.txt') as Julie:
+    data2 = Julie.readline()
+    Julie = data2.strip().split(',')
+
+with open('mikey.txt') as Mikey:
+    data3 = Mikey.readline()
+    Mikey = data3.strip().split(',')
+
+with open('sarah.txt') as Sarah:
+    data4 = Sarah.readline()
+    Sarah = data4.strip().split(',')
+    
+'''Create a new list by specifying the transformation that is applied 
+   to each of the data items within an existing list.'''
+print(sorted([sanitize(t) for t in James]))
+print(sorted([sanitize(t) for t in Julie]))
+print(sorted([sanitize(t) for t in Mikey]))
+print(sorted([sanitize(t) for t in Sarah]))

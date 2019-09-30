e3685f08b01eb88a61dce2d41b31deb39ec48bb3
Lucas Cavalare <lucascavalare@gmail.com>
2019-09-30 21:45:29 +0100

    Updating 3 highest values in list.

--- lists/athletes.py
+++ lists/athletes.py
@@ -12,7 +12,6 @@ def sanitize(time_string):
     (mins, secs) = time_string.split(splitter)
     return(mins + '.' + secs)
 
-
 ''' Open each of the data files in turn, read the line of data from the file, 
     and create a list from the line of data.''' 
 with open('james.txt') as James:        # Open the file.
@@ -31,35 +30,7 @@ with open('sarah.txt') as Sarah:
     data4 = Sarah.readline()
     Sarah = data4.strip().split(',')
     
-''' Replaces each of the data items within unordered and non-uniform data with the sorted, sanitized copy.'''
-James = sorted([sanitize(t) for t in James]))
-Julie = sorted([sanitize(t) for t in Julie]))
-Mikey = sorted([sanitize(t) for t in Mikey]))
-Sarah = sorted([sanitize(t) for t in Sarah]))
-
-''' Create new lists for each Athlete, and then populate it with the unique items found in each existing list.'''
-    
-unique_James = []
-    for each_t in James:                   # Iterate over the existing data. 
-        if each_t not in unique_James:     # If the data item ISN'T already in the new list.
-            unique_James.append(each_t)    # Append the unique data item to new list. 
-print(unique_James[0:3])                   # Slice the first three data items from the list and display them.  
-
-unique_Julie = []
-    for each_t in Julie:                   
-        if each_t not in unique_Julie:
-            unique_Julie.append(each_t)
-print(unique_Julie[0:3]) 
-
-unique_Mikey = []
-    for each_t in Mikey:                    
-        if each_t not in unique_Mikey:
-            unique_Mikey.append(each_t)
-print(unique_Mikey[0:3]) 
-
-unique_Sarah = []
-    for each_t in Sarah:                    
-        if each_t not in unique_Sarah:
-            unique_Sarah.append(each_t)
-print(unique_Sarah[0:3]) 
-
+print(sorted(set([sanitize(t) for t in James]))[0:3])
+print(sorted(set([sanitize(t) for t in Julie]))[0:3])
+print(sorted(set([sanitize(t) for t in Mikey]))[0:3])
+print(sorted(set([sanitize(t) for t in Sarah]))[0:3])
